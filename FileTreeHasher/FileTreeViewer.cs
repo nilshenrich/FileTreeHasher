f84ff31af34ca2aa2ab7206d44dc21e028dbdb51
Nils Henrich <nilshenrich@web.de>
2022-10-14 13:11:54 +0200

    Moved tree view data holding to new class and commented out everything not needed from my point of view

---
+++ FileTreeHasher/FileTreeViewer.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections.ObjectModel;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace FileTreeHasher
+{
+    public enum ExplorerItemType { Folder, File };
+
+    public class ExplorerItem //: INotifyPropertyChanged
+    {
+        //public event PropertyChangedEventHandler PropertyChanged;
+        public string Name { get; set; }
+        public ExplorerItemType Type { get; set; }
+        private ObservableCollection<ExplorerItem> m_children;
+        public ObservableCollection<ExplorerItem> Children
+        {
+            get
+            {
+                if (m_children == null)
+                {
+                    m_children = new ObservableCollection<ExplorerItem>();
+                }
+                return m_children;
+            }
+            set
+            {
+                m_children = value;
+            }
+        }
+
+        // Event triggers only needed if handlers available
+        //private bool m_isExpanded;
+        //public bool IsExpanded
+        //{
+        //    get { return m_isExpanded; }
+        //    set
+        //    {
+        //        if (m_isExpanded != value)
+        //        {
+        //            m_isExpanded = value;
+        //            NotifyPropertyChanged("IsExpanded");
+        //        }
+        //    }
+        //}
+
+        //private bool m_isSelected;
+        //public bool IsSelected
+        //{
+        //    get { return m_isSelected; }
+
+        //    set
+        //    {
+        //        if (m_isSelected != value)
+        //        {
+        //            m_isSelected = value;
+        //            NotifyPropertyChanged("IsSelected");
+        //        }
+        //    }
+
+        //}
+
+        //private void NotifyPropertyChanged(String propertyName)
+        //{
+        //    if (PropertyChanged != null)
+        //    {
+        //        PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        //    }
+        //}
+    }
+
+    class ExplorerItemTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate FolderTemplate { get; set; }
+        public DataTemplate FileTemplate { get; set; }
+
+        protected override DataTemplate SelectTemplateCore(object item)
+        {
+            var explorerItem = (ExplorerItem)item;
+            return explorerItem.Type == ExplorerItemType.Folder ? FolderTemplate : FileTemplate;
+        }
+    }
+}
--- FileTreeHasher/MainPage.xaml.cs
+++ FileTreeHasher/MainPage.xaml.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.ObjectModel;
-using System.ComponentModel;
-using Windows.UI.Xaml;
+﻿using System.Collections.ObjectModel;
 using Windows.UI.Xaml.Controls;
 
 // Die Elementvorlage "Leere Seite" wird unter https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x407 dokumentiert.
@@ -20,91 +17,24 @@ namespace FileTreeHasher
             DataSource = GetData();
         }
 
+        // Debugging
         private ObservableCollection<ExplorerItem> GetData()
         {
             ObservableCollection<ExplorerItem> data = new ObservableCollection<ExplorerItem>();
             ExplorerItem SampleTopLevelFolder = new ExplorerItem()
             {
                 Name = "Sample top level folder",
-                Type = ExplorerItem.ExplorerItemType.Folder
+                Type = ExplorerItemType.Folder
             };
+            ExplorerItem SampleNestedFile = new ExplorerItem()
+            {
+                Name = "Sample nested file",
+                Type = ExplorerItemType.File
+            };
+            SampleTopLevelFolder.Children.Add(SampleNestedFile);
             data.Add(SampleTopLevelFolder);
 
             return data;
-        }
-    }
-
-    public class ExplorerItem : INotifyPropertyChanged
-    {
-        public event PropertyChangedEventHandler PropertyChanged;
-        public enum ExplorerItemType { Folder, File };
-        public String Name { get; set; }
-        public ExplorerItemType Type { get; set; }
-        private ObservableCollection<ExplorerItem> m_children;
-        public ObservableCollection<ExplorerItem> Children
-        {
-            get
-            {
-                if (m_children == null)
-                {
-                    m_children = new ObservableCollection<ExplorerItem>();
-                }
-                return m_children;
-            }
-            set
-            {
-                m_children = value;
-            }
-        }
-
-        private bool m_isExpanded;
-        public bool IsExpanded
-        {
-            get { return m_isExpanded; }
-            set
-            {
-                if (m_isExpanded != value)
-                {
-                    m_isExpanded = value;
-                    NotifyPropertyChanged("IsExpanded");
-                }
-            }
-        }
-
-        private bool m_isSelected;
-        public bool IsSelected
-        {
-            get { return m_isSelected; }
-
-            set
-            {
-                if (m_isSelected != value)
-                {
-                    m_isSelected = value;
-                    NotifyPropertyChanged("IsSelected");
-                }
-            }
-
-        }
-
-        private void NotifyPropertyChanged(String propertyName)
-        {
-            if (PropertyChanged != null)
-            {
-                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
-            }
-        }
-    }
-
-    class ExplorerItemTemplateSelector : DataTemplateSelector
-    {
-        public DataTemplate FolderTemplate { get; set; }
-        public DataTemplate FileTemplate { get; set; }
-
-        protected override DataTemplate SelectTemplateCore(object item)
-        {
-            var explorerItem = (ExplorerItem)item;
-            return explorerItem.Type == ExplorerItem.ExplorerItemType.Folder ? FolderTemplate : FileTemplate;
         }
     }
 }
