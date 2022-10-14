7c67384d76cf8be24aa97b88636bb9d969c09240
Nils Henrich <nilshenrich@web.de>
2022-10-14 12:42:34 +0200

    Created tree view with data binding from example https://github.com/Microsoft/WinUI-Gallery/blob/1ecd85c908a8a1cb9a8201e548f58db379801e69/XamlControlsGallery/ControlPages/TreeViewPage.xaml

--- FileTreeHasher/MainPage.xaml.cs
+++ FileTreeHasher/MainPage.xaml.cs
@@ -1,18 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-using MUXC = Microsoft.UI.Xaml.Controls;
 
 // Die Elementvorlage "Leere Seite" wird unter https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x407 dokumentiert.
 
@@ -23,9 +13,98 @@ namespace FileTreeHasher
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private ObservableCollection<ExplorerItem> DataSource;
         public MainPage()
         {
             this.InitializeComponent();
+            DataSource = GetData();
+        }
+
+        private ObservableCollection<ExplorerItem> GetData()
+        {
+            ObservableCollection<ExplorerItem> data = new ObservableCollection<ExplorerItem>();
+            ExplorerItem SampleTopLevelFolder = new ExplorerItem()
+            {
+                Name = "Sample top level folder",
+                Type = ExplorerItem.ExplorerItemType.Folder
+            };
+            data.Add(SampleTopLevelFolder);
+
+            return data;
+        }
+    }
+
+    public class ExplorerItem : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public enum ExplorerItemType { Folder, File };
+        public String Name { get; set; }
+        public ExplorerItemType Type { get; set; }
+        private ObservableCollection<ExplorerItem> m_children;
+        public ObservableCollection<ExplorerItem> Children
+        {
+            get
+            {
+                if (m_children == null)
+                {
+                    m_children = new ObservableCollection<ExplorerItem>();
+                }
+                return m_children;
+            }
+            set
+            {
+                m_children = value;
+            }
+        }
+
+        private bool m_isExpanded;
+        public bool IsExpanded
+        {
+            get { return m_isExpanded; }
+            set
+            {
+                if (m_isExpanded != value)
+                {
+                    m_isExpanded = value;
+                    NotifyPropertyChanged("IsExpanded");
+                }
+            }
+        }
+
+        private bool m_isSelected;
+        public bool IsSelected
+        {
+            get { return m_isSelected; }
+
+            set
+            {
+                if (m_isSelected != value)
+                {
+                    m_isSelected = value;
+                    NotifyPropertyChanged("IsSelected");
+                }
+            }
+
+        }
+
+        private void NotifyPropertyChanged(String propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+
+    class ExplorerItemTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate FolderTemplate { get; set; }
+        public DataTemplate FileTemplate { get; set; }
+
+        protected override DataTemplate SelectTemplateCore(object item)
+        {
+            var explorerItem = (ExplorerItem)item;
+            return explorerItem.Type == ExplorerItem.ExplorerItemType.Folder ? FolderTemplate : FileTemplate;
         }
     }
 }
