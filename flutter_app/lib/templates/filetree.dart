75f192cc6d432042c6106a8ddf33e31ace457925
nilshenrich <nilshenrich@web.de>
2023-03-31 16:33:06 +0200

    Reduced used code

--- flutter_app/lib/templates/filetree.dart
+++ flutter_app/lib/templates/filetree.dart
@@ -17,15 +17,13 @@ import 'package:flutter_treeview/flutter_treeview.dart';
 // # Single file tree element (folder or file)
 // ##################################################
 abstract class T_FileTreeItem extends Node {
-  // Parameter
-  final String m_path;
-  final String m_name;
-  final IconData m_icon;
-
   // Constructor
-  const T_FileTreeItem(this.m_path, this.m_name, this.m_icon,
-      {List<T_FileTreeItem> content = const []})
-      : super(key: m_path, label: m_name, icon: m_icon, children: content);
+  const T_FileTreeItem(
+      {required String path,
+      required String name,
+      required IconData icon,
+      List<T_FileTreeItem> content = const []})
+      : super(key: path, label: name, icon: icon, children: content);
 }
 
 // ##################################################
@@ -33,14 +31,12 @@ abstract class T_FileTreeItem extends Node {
 // # Single folder view
 // ##################################################
 class T_FolderView extends T_FileTreeItem {
-  // Parameter
-  final String path;
-  final String name;
-
   // Constructor
-  const T_FolderView(this.path, this.name,
-      {List<T_FileTreeItem> content = const []})
-      : super(path, name, Icons.folder, content: content);
+  const T_FolderView(
+      {required String path,
+      required String name,
+      List<T_FileTreeItem> content = const []})
+      : super(path: path, name: name, icon: Icons.folder, content: content);
 }
 
 // ##################################################
@@ -48,12 +44,11 @@ class T_FolderView extends T_FileTreeItem {
 // # Single file view
 // ##################################################
 class T_FileView extends T_FileTreeItem {
-  // Parameter
-  final String path;
-  final String name;
-
   // Constructor
-  const T_FileView(this.path, this.name,
-      {String hashGen = "", String hashComp = ""})
-      : super(path, name, Icons.description);
+  const T_FileView(
+      {required String path,
+      required String name,
+      String hashGen = "",
+      String hashComp = ""})
+      : super(path: path, name: name, icon: Icons.description);
 }
