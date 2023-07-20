18eb9c8bef9e5cc6496e3beecb36c9561958d8ff
nilshenrich <nilshenrich@web.de>
2023-07-20 15:42:20 +0200

    Removed all repository files for gh-pages branch

--- flutter_app/android/app/src/main/kotlin/com/example/flutter_app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.file_tree_hasher
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- hooks/ApplyHooks.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Get root directory
-root_dir="$(git rev-parse --show-toplevel)"
-
-# Copy all hook files
-cp $root_dir/hooks/*-* $root_dir/.git/hooks/
-chmod 775 $root_dir/.git/hooks/*
