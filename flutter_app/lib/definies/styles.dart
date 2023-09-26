2233d1086a8150031daa7504b1fe2eaff5939871
Nils Henrich <nilshenrich@web.de>
2023-09-26 16:13:31 +0200

    <hook> Update to version 1.0.1

--- flutter_app/lib/definies/styles.dart
+++ flutter_app/lib/definies/styles.dart
@@ -2,7 +2,7 @@
 // # @file styles.dart
 // # @author Nils Henrich
 // # @brief General widget styles
-// # @version 1.0.0+4
+// # @version 1.0.1
 // # @date 2023-04-03
 // #
 // # @copyright Copyright (c) 2023
